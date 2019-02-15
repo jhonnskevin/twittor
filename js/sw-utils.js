b9a192c24e5fb058ba8bf6b002514b6a543f1575
jhonnskevin <jhons.crespo@gmail.com>
2019-02-15 15:57:09 -0400

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+
+//guardar en el cache dinamico
+function actualizaCacheDinamico(dynamicCache, req, res){
+
+    if(res.ok){
+        caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+}
